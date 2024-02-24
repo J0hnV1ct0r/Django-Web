a09efbc6f5f0959c574db663f0d40cf5a8e05ad5
victor <j1747victor@gmail.com>
2024-02-24 14:05:35 -0300

    sistema de login

--- dreaming/base/urls.py
+++ dreaming/base/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import WorkList, WorkDetail, WorkCreate
+from .views import WorkList, WorkDetail, WorkCreate, WorkUpdate, DeleteView, CustomLoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('', WorkList.as_view(), name='works'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('works', WorkList.as_view(), name='works'),
     path('work/<int:pk>/', WorkDetail.as_view(), name='work'),
     path('work-create', WorkCreate.as_view(), name='work-create'),
+    path('work-update/<int:pk>/', WorkUpdate.as_view(), name='work-update'),
+    path('work-delete/<int:pk>/', DeleteView.as_view(), name='work-delete'),
 ]
--- dreaming/base/views.py
+++ dreaming/base/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
 from .models import Work
 
 
 # Create your views here.
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('works')
+
+
 class WorkList(ListView):
     model = Work
     context_object_name = 'works'
@@ -21,4 +31,16 @@ class WorkDetail(DetailView):
 class WorkCreate(CreateView):
     model = Work
     fields = '__all__'
+    success_url = reverse_lazy('works')
+
+
+class WorkUpdate(UpdateView):
+    model = Work
+    fields = '__all__'
+    success_url = reverse_lazy('works')
+
+
+class WorkDelete(DeleteView):
+    model = Work
+    context_object_name = 'work'
     success_url = reverse_lazy('works')
