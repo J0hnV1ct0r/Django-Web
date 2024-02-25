aeb3e55bc96e07fe2120308acea705744297d033
victor <j1747victor@gmail.com>
2024-02-25 18:07:19 -0300

    ajustes finais do sistema de login e cadastro

--- dreaming/base/urls.py
+++ dreaming/base/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import WorkList, WorkDetail, WorkCreate, WorkUpdate, DeleteView, CustomLoginView
+from .views import WorkList, WorkDetail, WorkCreate, WorkUpdate, DeleteView, CustomLoginView, RegisterPage
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
-    path('works', WorkList.as_view(), name='works'),
+    path('register/', RegisterPage.as_view(), name='register'),
+    path('', WorkList.as_view(), name='works'),
     path('work/<int:pk>/', WorkDetail.as_view(), name='work'),
     path('work-create', WorkCreate.as_view(), name='work-create'),
     path('work-update/<int:pk>/', WorkUpdate.as_view(), name='work-update'),
--- dreaming/base/views.py
+++ dreaming/base/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.forms import BaseModelForm
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
 from .models import Work
 
 
@@ -17,30 +22,48 @@ class CustomLoginView(LoginView):
         return reverse_lazy('works')
 
 
-class WorkList(ListView):
+class RegisterPage(FormView):
+    template_name = 'base/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('works')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('works')
+        return super(RegisterPage, self).get(*args, **kwargs)
+
+
+class WorkList(LoginRequiredMixin, ListView):
     model = Work
     context_object_name = 'works'
 
 
-class WorkDetail(DetailView):
+class WorkDetail(LoginRequiredMixin, DetailView):
     model = Work
     context_object_name = 'work'
     template_name = 'base/work.html'
 
 
-class WorkCreate(CreateView):
+class WorkCreate(LoginRequiredMixin, CreateView):
     model = Work
     fields = '__all__'
     success_url = reverse_lazy('works')
 
 
-class WorkUpdate(UpdateView):
+class WorkUpdate(LoginRequiredMixin, UpdateView):
     model = Work
     fields = '__all__'
     success_url = reverse_lazy('works')
 
 
-class WorkDelete(DeleteView):
+class WorkDelete(LoginRequiredMixin, DeleteView):
     model = Work
     context_object_name = 'work'
     success_url = reverse_lazy('works')
